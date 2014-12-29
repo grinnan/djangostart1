cb494ca0acf5e0e014d02c39ca018c8b21c58f68
grinnan <cgrinnan@gmail.com>
2014-12-29 08:25:44 -0800

    init

---
+++ wercksdemo/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import Template, Context
+from wercksdemo.models import City
+
+def home(request):
+    template = Template(
+        """Hello, world! Here are all the cities of the world:
+{% for city in cities %}{{city.name}}{% endfor %}"""
+    )
+
+    cities = City.objects.all()
+    context = Context({"cities": cities})
+
+    return HttpResponse(template.render(context))
